170038833c11cdc81d69c76381d0fa41e67d0730
Sly Kint A. Bacalso <slykint.bacalso@cit.edu>
2023-03-09 10:55:26 +0800

    Add code docs for Utils proj

--- CharacterRecognitionPerceptron.Utils/Array.cs
+++ CharacterRecognitionPerceptron.Utils/Array.cs
@@ -1,12 +1,26 @@
 ﻿namespace CharacterRecognitionPerceptron.Utils
 {
+    /// <summary>
+    /// Array helper class.
+    /// </summary>
+    /// <typeparam name="T"></typeparam>
     public class Array<T>
     {
+        /// <summary>
+        /// Creates an array of size 'size' with all elements set to 0.
+        /// </summary>
+        /// <param name="size"></param>
+        /// <returns>Array of zeroes</returns>
         public static T[] AllZeroes(int size)
         {
             return new T[size];
         }
 
+        /// <summary>
+        /// Creates an array of size 'size' using a normal distribution.
+        /// </summary>
+        /// <param name="size"></param>
+        /// <returns>Randomized normal distribution array</returns>
         public static T[] NormalDistribution(int size)
         {
             var rand = new Random();
